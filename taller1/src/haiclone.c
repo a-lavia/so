e361b13843b55690e51b31b5987e658a4632ec30
Alejandro <lavia.alejandro@gmail.com>
2016-08-18 20:38:40 -0300

    taller 1 so

---
+++ taller1/src/haiclone.c
@@ -0,0 +1,63 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+pid_t child;
+int count;
+
+void SIGURG_HANDLER() {
+	printf("ya va! \n");
+	count++;
+}
+
+void SIGINT_HANDLER() {
+	if(count < 5) {
+		printf("sup! \n");
+		kill(child, SIGURG);
+		sleep(1);
+		count++;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int status;
+	pid_t parent = getpid();
+
+	count = 0;
+
+	if (argc <= 1) {
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
+		exit(1);
+	}
+
+  signal(SIGINT, &SIGINT_HANDLER);
+
+	/* Fork en dos procesos */
+	child = fork();
+	if (child == -1) { perror("ERROR fork"); return 1; }
+	if (child == 0) {
+
+    signal(SIGURG, &SIGURG_HANDLER);
+
+    while(1) {
+			if(count == 5) {
+				/* Solo se ejecuta en el hijo */
+				execvp(argv[1], argv+1);
+				/* Si vuelve de exec() hubo un error */
+				perror("ERROR child exec(...)"); exit(1);
+			} else {
+				kill(parent, SIGINT);
+			}
+    }
+	} else {
+		/* Solo se ejecuta en el padre */
+		while(1) {
+			if (wait(&status) < 0) { perror("waitpid"); break; }
+			if (WIFEXITED(status)) break; /* Proceso terminado */
+		}
+
+	}
+	return 0;
+}
