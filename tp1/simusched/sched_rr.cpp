fe51af0a5cf55b9a12f4cf2a95999d8119b13b8b
Joel <joel.e.camera@gmail.com>
2016-08-31 00:04:31 -0300

    Ahora el scheduler round-robin funciona. Las imagenes ahora van a la carpeta imagenes del informe.

--- tp1/simusched/sched_rr.cpp
+++ tp1/simusched/sched_rr.cpp
@@ -10,8 +10,9 @@ SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
 
 	// Guardo en un vector los valores de los quantums que estan asociados a los cores
-	for(int i = 1; i <= argn[0]; i++)
+	for(int i = 1; i <= argn[0]; i++){
 		this->quantum_por_core.push_back(argn[i]);
+	}
 
 	// Copio los valores de los quantums al vector que lo va a asociar a cada tarea
 	// para que vaya disminuyendo por la cantidad de ticks
@@ -35,47 +36,33 @@ void SchedRR::unblock(int pid) {
 int SchedRR::tick(int cpu, const enum Motivo m) {
 	int prox_pid = IDLE_TASK;
 
-	if(m == EXIT){
-		// Terminó su ejecución, pasa a la proxima. Actualizo el quantum para la proxima tarea.
-		if(!(this->cola_procesos.empty())) {
+	if(current_pid(cpu) == IDLE_TASK){
+		if(!this->cola_procesos.empty()){
 			prox_pid = this->cola_procesos.front();
 			this->cola_procesos.pop();
+			this->quantum_proceso[cpu] = this->quantum_por_core[cpu];
 		}
-		// Actualizo el quantum aunque no haya ninguna tarea
-		this->quantum_proceso[cpu] = this->quantum_por_core[cpu];
 	
-	} else if(m == BLOCK){
-		// Se bloqueó un proceso, devuelvo un proceso nuevo de la pila para que termine el quantum
-		prox_pid = this->cola_procesos.front();
-		this->cola_procesos.pop();
-
 	} else {
-		// Si es la tarea NO es idle: actualizo el quantum y reviso si se le terminó o no.
-		if(current_pid(cpu) != IDLE_TASK){
-			// La tarea no termino. Descuento el quantum.
+		if(m == TICK){
 			this->quantum_proceso[cpu]--;
-		
-			// Reviso si termino el quantum que tenía.
-			if(this->quantum_proceso[cpu] <= 0){
-				//Terminó su quantum, lo actualizo, la encolo de nuevo y desencolo la nueva.
+			prox_pid = current_pid(cpu);
+
+			if(this->quantum_proceso[cpu] == 0){
+				if(!this->cola_procesos.empty()){
+					this->cola_procesos.push(prox_pid);
+					prox_pid = this->cola_procesos.front();
+					this->cola_procesos.pop();
+				}
+
 				this->quantum_proceso[cpu] = this->quantum_por_core[cpu];
-				
-				int pid_tarea_a_encolar = current_pid(cpu);
-				
-				this->cola_procesos.push(pid_tarea_a_encolar);
-				
-				prox_pid = this->cola_procesos.front();
-				
-				this->cola_procesos.pop();
-			} else {
-				// No terminó su quantum, continua
-				prox_pid = current_pid(cpu);
 			}
+		
 		} else {
-			// La tarea es idle, me fijo si hay una tarea nueva en la cola, si la hay la ejecuto y actualizo el quantum
-			if(this->cola_procesos.size() > 1){
+			if(!this->cola_procesos.empty()){
 				prox_pid = this->cola_procesos.front();
 				this->cola_procesos.pop();
+				this->quantum_proceso[cpu] = this->quantum_por_core[cpu];
 			}
 		}
 	}
