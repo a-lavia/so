741e3a19d9ad69c56b2fe8c4176ec9c94d9f73f1
Alejandro Norberto Lavia <alavia@ws10.labo1>
2016-09-02 00:32:03 -0300

    Implementacion parcial de rsjf

--- tp1/simusched/sched_rsjf.cpp
+++ tp1/simusched/sched_rsjf.cpp
@@ -3,23 +3,59 @@
 using namespace std;
 
 SchedRSJF::SchedRSJF(vector<int> argn) {
-        // Recibe la cantidad de cores y sus cpu_quantum por parámetro
-/* llenar */
+	// Recibe la cantidad de cores y sus cpu_quantum por parámetro
+	for(int i = 1; i <= argn[0]; i++) {
+		this->quantum_core_base.push_back(argn[i]);
+		this->quantum_core_actual.push_back(argn[i]);
+		this->proceso_en_core.push_back(NULL);
+	}
+
+	for(unsigned int i = argn[0]+1; i < argn.size(); i++)
+		this->tiempo_proceso.push(argn[i]);
+
 }
 
 SchedRSJF::~SchedRSJF() {
-/* llenar */
+
 }
 
 void SchedRSJF::load(int pid) {
-/* llenar */
+	proceso nuevoProceso = proceso(pid, this->tiempo_proceso.front());
+	this->tiempo_proceso.pop();
+	cola_procesos.push(nuevoProceso);
 }
 
 void SchedRSJF::unblock(int pid) {
-/* llenar */
+	//No se bloquean
 }
 
 int SchedRSJF::tick(int core, const enum Motivo m) {
-/* llenar */
+	switch (m) {
+		case TICK:
+			//FIJARSE SI ES IDLE
+			//Me fijo si hay un proceso corriendo en el core actualmente
+			if(proceso_en_core[core] != NULL) {
+
+				//Le descontamos el tiempo al proceso, y disminuimos el quantum
+				proceso_en_core[core]->tiempo--;
+				quantum_core_actual[core]--;
+
+				//Se termino el proceso
+				if(proceso_en_core[core]->tiempo == 0) {
+					//No agregarlo a la cola, obtener el siguiente
+				} else if(quantum_core_actual[core] == 0) { //Se termino su quantum
+					quantum_core_actual[core] = quantum_core_base[core];
+
+				}
+
+			} else {
+				//Obtenemos el proceso de la cola
+			}
+			break;
+
+
+		default:
+			break;
+	}
 	return 0;
 }
--- tp1/simusched/sched_rsjf.h
+++ tp1/simusched/sched_rsjf.h
@@ -8,17 +8,34 @@
 
 using namespace std;
 
+struct proceso {
+	int pid;
+	int tiempo;
+
+	proceso(int p, int t) : pid(p), tiempo(t) {}
+
+	bool operator <(const proceso& other) const {
+    return this->tiempo < other.tiempo;
+	}
+
+};
+
 class SchedRSJF : public SchedBase {
 	public:
 		SchedRSJF(std::vector<int> argn);
-        ~SchedRSJF();
+    ~SchedRSJF();
 		virtual void initialize() {};
 		virtual void load(int pid);
 		virtual void unblock(int pid);
-		virtual int tick(int cpu, const enum Motivo m);	
+		virtual int tick(int cpu, const enum Motivo m);
 	private:
-/* llenar */
-	
+
+		vector<int> quantum_core_base;
+		vector<int> quantum_core_actual;
+		vector<proceso*> proceso_en_core;
+
+		queue<int> tiempo_proceso;
+		priority_queue<proceso> cola_procesos;
 };
 
 #endif
