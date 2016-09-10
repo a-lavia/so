145666c16eb29bab38f9baf175c40669a4ed3188
Joel <joel.e.camera@gmail.com>
2016-09-10 13:15:59 -0300

    Taller 2: Ejercicio 1 hecho

--- taller2/taller-ipc/ejercicios/mini-telnet-1/mt_client.c
+++ taller2/taller-ipc/ejercicios/mini-telnet-1/mt_client.c
@@ -1,7 +1,50 @@
 #include "mt.h"
 
-int main(int argc, char* argv[]) {
-    /* TODO: Implementar */
+#define ADDR_LOCAL "127.000.000.001"
 
-    return 0;
+int main(int argc, char* argv[]) {
+	int                   socket_fd;
+	// int                   len;
+	struct sockaddr_in    remote;
+	char                  buf[MAX_MSG_LENGTH];
+	struct in_addr        dir_local;
+
+	/* Crear un socket de tipo UNIX con SOCK_STREAM */
+	if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 1){
+		perror("Error creando socket");
+		exit(1);
+	}
+
+	/* Resuelvo la direccion IP */
+	if((inet_aton(ADDR_LOCAL, &dir_local)) == 0){
+		perror("Error al intentar resolver dirección IP");
+		exit(1);
+	}
+
+
+	/* Establecer la dirección a la cual conectarse. */
+	remote.sin_family = AF_INET;
+	remote.sin_port = htons(PORT);
+	remote.sin_addr = dir_local;
+
+	// len = sizeof(struct sockaddr_in);
+
+	/* Conectarse */
+	if(connect(socket_fd, (struct sockaddr *)&remote, sizeof(remote)) == 1){
+		perror("Error conectandose");
+		exit(1);
+	}
+
+	/* Establecer la dirección a la cual conectarse para escuchar. */
+	while(printf("> "), fgets(buf, MAX_MSG_LENGTH, stdin), !feof(stdin)){
+		if(sendto(socket_fd, buf, strlen(buf), 0, (struct sockaddr *)&remote, sizeof(remote)) == -1){
+			perror("Error enviando");
+			exit(1);
+		}
+	}
+
+	/* Cerrar el socket. */
+	close(socket_fd);
+
+	return 0;
 }
